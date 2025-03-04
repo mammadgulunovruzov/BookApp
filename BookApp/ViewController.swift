623e2b5ec55f5d2a3b622c10b976e0a914524568
Mammadgulu Novruzov <mnovruzov2021@ada.edu.az>
2025-03-04 14:42:47 +0100

    Initial Commit

---
+++ BookApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookApp
+//
+//  Created by Mammadgulu Novruzov on 04.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
